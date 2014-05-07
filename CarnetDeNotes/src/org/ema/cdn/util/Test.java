9f1a7f819689c6ae5981b0c4fa29a1d4b91cc74e
eddyEMA <eddy.gharbi@laposte.net>
2014-05-07 13:49:05 +0200

    Suppression old version interface

--- CarnetDeNotes/src/org/ema/cdn/util/Test.java
+++ CarnetDeNotes/src/org/ema/cdn/util/Test.java
@@ -1,15 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.ema.cdn.util;
 
-/**
- *
- * @author Eddy
- */
-public class Test {
-    
+import java.sql.*;
+
+
+
+
+public class Test
+{
+
+
+
+public static void main(String[] args)
+{
+
+
+try
+{
+
+Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+}
+
+catch(Exception e)
+{
+e.printStackTrace();
+}
+
+try
+{
+//Connection conn = DriverManager.getConnection("jdbc:mysql://?user=root[localhost]/[testJDBC]");
+Connection conn = DriverManager.getConnection("jdbc:mysql:///");
+
+Statement stat = conn.createStatement();
+stat.executeUpdate("testJDBC");
+}
+catch(SQLException e){
+    System.out.println("sql exception");
+    while (e!=null){
+      System.out.println(e.getErrorCode());
+      System.out.println(e.getMessage());
+      System.out.println(e.getSQLState());
+      e.printStackTrace();
+      e=e.getNextException();}
+    }
+catch(Exception e)
+	{
+	e.printStackTrace();
+	}
+}
+
 }
