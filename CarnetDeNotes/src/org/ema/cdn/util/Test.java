c014c28ea09fb4cf84e57455f68b841fbec0667a
eddyEMA <eddy.gharbi@laposte.net>
2014-05-10 09:13:31 +0200

    Acces BDD

--- CarnetDeNotes/src/org/ema/cdn/util/Test.java
+++ CarnetDeNotes/src/org/ema/cdn/util/Test.java
@@ -2,51 +2,12 @@ package org.ema.cdn.util;
 
 import java.sql.*;
 
-
-
-
-public class Test
-{
-
-
-
-public static void main(String[] args)
-{
-
-
-try
-{
-
-Class.forName("com.mysql.jdbc.Driver").newInstance();
-
-}
-
-catch(Exception e)
-{
-e.printStackTrace();
-}
-
-try
-{
-//Connection conn = DriverManager.getConnection("jdbc:mysql://?user=root[localhost]/[testJDBC]");
-Connection conn = DriverManager.getConnection("jdbc:mysql:///");
-
-Statement stat = conn.createStatement();
-stat.executeUpdate("testJDBC");
-}
-catch(SQLException e){
-    System.out.println("sql exception");
-    while (e!=null){
-      System.out.println(e.getErrorCode());
-      System.out.println(e.getMessage());
-      System.out.println(e.getSQLState());
-      e.printStackTrace();
-      e=e.getNextException();}
+public class Test{
+    public static void main(String[] args) throws SQLException{
+        AccesBDD BDD = new AccesBDD();
+        BDD.AfficheRequete();
+        int nombreResultat = BDD.NombreLignes();
+        System.out.println("\n\nNombre de resultat trouvé : " + nombreResultat);
+        
     }
-catch(Exception e)
-	{
-	e.printStackTrace();
-	}
-}
-
 }
