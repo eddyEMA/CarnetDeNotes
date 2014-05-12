db40f104b4ad4c7d9563b0b3b4394c75fa2e514d
eddyEMA <eddy.gharbi@laposte.net>
2014-05-12 14:45:51 +0200

    Commit de fou
    :)

---
+++ CarnetDeNotes/src/org/ema/cdn/metier/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.ema.cdn.metier;
+
+/**
+ *
+ * @author Eddy
+ */
+public class Main {
+    //public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		System.out.print("Hello World !");
+//	}
+}
