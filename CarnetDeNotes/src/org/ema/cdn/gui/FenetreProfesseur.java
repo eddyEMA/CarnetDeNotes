f031c922d5d99b7e1ec81e84f3b72e9d32233257
eddyEMA <eddy.gharbi@laposte.net>
2014-05-27 10:12:39 +0200

    TestDuDaoCours

--- CarnetDeNotes/src/org/ema/cdn/gui/FenetreProfesseur.java
+++ CarnetDeNotes/src/org/ema/cdn/gui/FenetreProfesseur.java
@@ -6,10 +6,23 @@
 
 package org.ema.cdn.gui;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author eddy
  */
 public class FenetreProfesseur extends Fenetre{
-    
+    public FenetreProfesseur(){
+//     TableauProf monTab = new TableauProf();
+//     this.add(monTab.CreerJTableProf());
+     Onglet monOnglet = new Onglet();
+     monOnglet.insererTableau("MonPremierOnglet", null);
+     this.setSize(700,400);
+     this.setLocationRelativeTo(null);
+     this.setUndecorated(false);
+     this.setResizable(true);
+     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     this.setVisible(true);
+    }
 }
